67a8334dfaf31788c7b632bad2e872693cc38c84
blue <blue@MacMini.local>
2023-04-14 05:35:24 +0800

    first commit

---
+++ rules.json.js
@@ -0,0 +1,59 @@
+{
+  "gfwlist": {
+    "name": "gfwlist.conf",
+    "date": "2023-04-14 05:31",
+    "md5": "cc08924f36812101d97d42c438d394c7",
+    "count": "    5563"
+  },
+  "chnroute": {
+    "name": "chnroute.txt",
+    "date": "2023-04-14 05:31",
+    "md5": "50556739ec4273a2939e61a7de54437c",
+    "count": "    3604",
+    "count_ip": "8889845720",
+    "source": "misakaio",
+    "url": "https://github.com/misakaio/chnroutes2/blob/master/chnroutes.txt"
+  },
+  "chnroute2": {
+    "name": "chnroute2.txt",
+    "date": "2023-04-14 05:31",
+    "md5": "ad340381965489f6342914db3963a3e1",
+    "count": "    6397",
+    "count_ip": "142088743050",
+    "source": "ipip",
+    "url": "https://github.com/firehol/blocklist-ipsets/blob/master/ipip_country/ipip_country_cn.netset"
+  },
+  "chnroute3": {
+    "name": "chnroute3.txt",
+    "date": "2023-04-14 05:31",
+    "md5": "0c28223b45a2a09a88781016eb37a4cb",
+    "count": "    8608",
+    "count_ip": "343168448",
+    "source": "apnic",
+    "url": "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+  },
+  "cdn_china": {
+    "name": "cdn.txt",
+    "date": "2023-04-14 05:31",
+    "md5": "8c6e06be429f362fb6dacd63c2c515bf",
+    "count": "   64198"
+  },
+  "apple_china": {
+    "name": "apple_china.txt",
+    "date": "2023-04-14 05:31",
+    "md5": "47e7c811b21fd29029a11c8d5c839669",
+    "count": "     137"
+  },
+  "google_china": {
+    "name": "google_china.txt",
+    "date": "2023-04-14 05:31",
+    "md5": "be793a76112b26d687d4f854a09bf68a",
+    "count": "     161"
+  },
+  "cdn_test": {
+    "name": "cdn_test.txt",
+    "date": "2023-04-14 05:31",
+    "md5": "576a8d266539be4d7885e665f9a3f7cf",
+    "count": "      81"
+  }
+}
