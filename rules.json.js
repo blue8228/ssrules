b834ac001881efe3894f9ecfc83273b9f4fa4690
root <root@pc-asus>
2023-04-28 12:02:31 +0800

    wsl更新规则

--- rules.json.js
+++ rules.json.js
@@ -1,22 +1,22 @@
 {
   "gfwlist": {
     "name": "gfwlist.conf",
-    "date": "2023-04-23 20:06",
+    "date": "2023-04-28 12:01",
     "md5": "f792cb75b0d60ebaefdd04a31627d8de",
     "count": "5560"
   },
   "chnroute": {
     "name": "chnroute.txt",
-    "date": "2023-04-23 20:06",
-    "md5": "219b9af64dc12aca854965ad3acf38c4",
-    "count": "3604",
-    "count_ip": "299913432",
+    "date": "2023-04-28 12:01",
+    "md5": "7d0c08aebce410fc74b932b4ba26b138",
+    "count": "3594",
+    "count_ip": "299886060",
     "source": "misakaio",
     "url": "https://github.com/misakaio/chnroutes2/blob/master/chnroutes.txt"
   },
   "chnroute2": {
     "name": "chnroute2.txt",
-    "date": "2023-04-18 11:37",
+    "date": "2023-04-28 12:01",
     "md5": "129cb57658181d8948ace37f35bbca68",
     "count": "6368",
     "count_ip": "17534691524",
@@ -25,34 +25,34 @@
   },
   "chnroute3": {
     "name": "chnroute3.txt",
-    "date": "2023-04-18 11:37",
-    "md5": "8a4521a14b03c8102f724885666b5254",
-    "count": "8610",
-    "count_ip": "343169212",
+    "date": "2023-04-28 12:01",
+    "md5": "9d9bf08599180c427ad9ab6cd0dfb7c4",
+    "count": "8609",
+    "count_ip": "343103166",
     "source": "apnic",
     "url": "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
   },
   "cdn_china": {
     "name": "cdn.txt",
-    "date": "2023-04-23 20:06",
-    "md5": "859c8d501c56fa1aac96bf6361707944",
-    "count": "64422"
+    "date": "2023-04-28 12:01",
+    "md5": "8e4f8b18f8da6dc6a005ed213ea0f2fb",
+    "count": "64429"
   },
   "apple_china": {
     "name": "apple_china.txt",
-    "date": "2023-04-18 11:37",
+    "date": "2023-04-28 12:01",
     "md5": "77be5ca1c9910bdbbc2d529811bee2d0",
     "count": "137"
   },
   "google_china": {
     "name": "google_china.txt",
-    "date": "2023-04-23 20:06",
+    "date": "2023-04-28 12:01",
     "md5": "2acb00542a8e264052db7a42b3a693d8",
     "count": "159"
   },
   "cdn_test": {
     "name": "cdn_test.txt",
-    "date": "2023-04-18 11:37",
+    "date": "2023-04-28 12:01",
     "md5": "576a8d266539be4d7885e665f9a3f7cf",
     "count": "81"
   }
